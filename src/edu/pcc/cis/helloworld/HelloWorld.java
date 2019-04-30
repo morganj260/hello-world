392db21b085c40d7c53c9601efdfc0831aec864e
Morgan Jarvis <morgan.jarvis@pcc.edu>
2019-04-29 23:22:15 -0700

    added main that print hello, world!

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,13 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Display hello world on the screen.
+ *
+ * @author Morgan Jarvis
+ * @version 2019.04.29
+ */
 public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
 }
